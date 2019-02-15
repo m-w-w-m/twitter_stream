929bcbe0771a06bd5d56845ee896ef68fae06ad4
Mike Wess <mkwesselhoff@gmail.com>
2019-02-15 20:46:52 +0100

    stripped data down to only tweet, and save each tweet to cvs file.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from tweepy import Stream
 from tweepy import OAuthHandler
 from tweepy.streaming import StreamListener
+import time
+
 
 ckey = 'H237cHGshuhFS6dPQwsAVET8G'
 csecret = 'vtlY6yL6yEzXZPCdZIVEygzQgo0MfXdHoZJgOwhpoFlK4m7Jky'
@@ -10,7 +12,14 @@ asecret = 'vIUL6pXX3aZTKJ5wYJRxhqjqCiI0L0uw1jgGYLL24EcTV'
 class Listener(StreamListener):
 
 	def on_data(self, data):
-		print(data)
+		tweet = data.split(',"text":"')[1].split('","source')[0]
+		print(tweet)
+
+		saveThis = str(time.time())  + '::' + tweet
+		saveFile = open('twitDB2.csv', 'a')
+		saveFile.write(saveThis)
+		saveFile.write('\n')
+		saveFile.close()
 		return True
 
 	def on_error(self, status):
